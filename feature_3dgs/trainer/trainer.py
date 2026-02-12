99c85148000d995e0ec6491512956c967839a4d3
yindaheng98 <yindaheng98@gmail.com>
2026-02-11 16:31:54 -0800

    fixed FeatureTrainer

--- feature_3dgs/trainer/trainer.py
+++ feature_3dgs/trainer/trainer.py
@@ -1,43 +1,24 @@
 import torch
-from gaussian_splatting import Camera
 from gaussian_splatting.utils import l1_loss
 from gaussian_splatting.trainer import TrainerWrapper, AbstractTrainer
-from feature_3dgs.extractor import AbstractFeatureExtractor
-from feature_3dgs import FeatureGaussian
+from feature_3dgs import FeatureGaussianModel
+from feature_3dgs.dataset import FeatureCamera
+
 
 class FeatureTrainer(TrainerWrapper):
     def __init__(
-        self,
-        base_trainer: AbstractTrainer,
-        extractor: AbstractFeatureExtractor
+            self,  base_trainer: AbstractTrainer,
+            feature_l1_weight=1.0,
     ):
         super().__init__(base_trainer=base_trainer)
-        self.optimizer.add_param_group([{"lr":0.0001, "params":extractor.parameters()}])
-        self.extractor = extractor
-        if isinstance(self.model, FeatureGaussian) and self.model.get_decoder is not None:
-            self.optimizer.add_param_group([{"lr":0.0001, "params":self.model.get_decoder.parameters()}])
+        model = self.model
+        assert isinstance(model, FeatureGaussianModel), "FeatureTrainer's model must be a FeatureGaussianModel"
+        self.optimizer.add_param_group([{"lr": 0.0001, "params": model.get_decoder.parameters()}])
+        self.feature_l1_weight = feature_l1_weight
 
-
-    def loss(self, out: dict, camera: Camera) -> torch.Tensor:
-        loss = self.base_trainer.loss(out, camera)
-        feature_map_3dgs = out['feature_map']
-        feature_map_extractor = self.extractor(camera.ground_truth_image)
-        feature_map_loss = l1_loss(feature_map_3dgs, feature_map_extractor) 
-        return loss + feature_map_loss
-
-    def update_learning_rate(self):
-        self.optim_step()
-
-    def step(self, camera: Camera):
-        self.update_learning_rate()
-        camera = self.preprocess(camera)
-        out = self.model(camera)
-        loss = self.loss(out, camera)
-        loss.backward()
-        self.before_optim_hook(loss=loss, out=out, camera=camera)
-        self.optim_step()
-        self.after_optim_hook(loss=loss, out=out, camera=camera)
-        return loss, out
-
-    def extract_features(self, input):
-        return self.extractor.extract(input)
+    def loss(self, out: dict, camera: FeatureCamera) -> torch.Tensor:
+        loss = super().loss(out, camera)
+        render = out['feature_map']
+        gt = camera.feature_map
+        feature_loss = l1_loss(render, gt)
+        return loss + feature_loss * self.feature_l1_weight
